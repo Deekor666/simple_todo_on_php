48b747154070131d35101355def57bf5898b4b9f
E.Korolev <e.korolev@techmoney.ru>
2023-03-14 15:12:34 +0700

    migration db

---
+++ migration.php
@@ -0,0 +1,24 @@
+<?php
+
+$database = new \App\Database();
+
+$sqlTasks = "create table myapp.tasks
+(
+    id       int auto_increment
+        primary key,
+    text     text                 null,
+    username mediumtext           null,
+    status   tinyint(1) default 0 null
+);";
+
+$sqlUsers = "create table myapp.users
+(
+    id       int auto_increment
+        primary key,
+    password mediumtext null,
+    username mediumtext null
+);";
+
+// Выполняем SQL запрос
+$database->connect->query($sqlUsers);
+$database->connect->query($sqlTasks);
