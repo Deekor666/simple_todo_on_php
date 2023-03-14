c309ad3febb1e0613218215edd852b59bacfe8f3
E.Korolev <e.korolev@techmoney.ru>
2023-03-14 14:46:28 +0700

    .env path fix

--- src/Database.php
+++ src/Database.php
@@ -28,7 +28,7 @@ class Database
 
     private static function loadEnv()
     {
-        $dotenv = fopen('../.env', 'r');
+        $dotenv = fopen(__DIR__ . '/../.env', 'r');
         while ($line = fgets($dotenv)) {
             putenv(trim($line));
         }
