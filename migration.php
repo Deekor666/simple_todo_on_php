aa9befb8d4cfde0ab1bccfee9de4cb4e903b373d
E.Korolev <e.korolev@techmoney.ru>
2023-03-14 15:23:50 +0700

    migration db

--- migration.php
+++ migration.php
@@ -3,8 +3,16 @@
 use App\Database;
 
 require_once __DIR__ . '/src/Database.php';
+function loadEnv()
+{
+    $dotenv = fopen(__DIR__ . '/.env', 'r');
+    while ($line = fgets($dotenv)) {
+        putenv(trim($line));
+    }
+    fclose($dotenv);
+}
 
-Database::loadEnv();
+loadEnv();
 $host = getenv('DB_HOST');
 $dbname = getenv('DB_NAME');
 $username = getenv('DB_USER');
@@ -12,9 +20,9 @@ $password = getenv('DB_PASSWORD');
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 $pdo = new PDO($dsn, $username, $password, $options);
 
